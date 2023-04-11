993d0d7adc09e464873daa0fa845b5bc6b83ac0c
Cameron Schloer <cameronschloer@gmail.com>
2023-04-11 14:23:42 -0400

    Fixed frame rate in png to mp4 converter

--- convert_argos_pngs_to_mp4.bash
+++ convert_argos_pngs_to_mp4.bash
@@ -2,4 +2,4 @@
 
 for filename in *.png; do      [ -f "$filename" ] || continue;     mv "$filename" "${filename//frame_/}"; done
 
-ffmpeg -r 60 -f image2 -s 1299x922 -i %10d.png -vcodec libx264 -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" -crf 25  -pix_fmt yuv420p argos_recording.mp4
+ffmpeg -r 10 -f image2 -s 1299x922 -i %10d.png -vcodec libx264 -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" -crf 25  -pix_fmt yuv420p argos_recording.mp4
