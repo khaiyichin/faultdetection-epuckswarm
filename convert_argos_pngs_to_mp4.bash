88dd8f26f04af14c6fc7c433437cefd41dc279be
Cameron Schloer <cameronschloer@gmail.com>
2023-04-11 15:32:02 -0400

    Set mp4 convenience script to delete frames automatically after

--- convert_argos_pngs_to_mp4.bash
+++ convert_argos_pngs_to_mp4.bash
@@ -3,3 +3,5 @@
 for filename in *.png; do      [ -f "$filename" ] || continue;     mv "$filename" "${filename//frame_/}"; done
 
 ffmpeg -r 10 -f image2 -s 1299x922 -i %10d.png -vcodec libx264 -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" -crf 25  -pix_fmt yuv420p argos_recording.mp4
+
+rm -rf *.png
