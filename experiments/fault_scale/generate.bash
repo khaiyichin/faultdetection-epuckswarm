c926cdef69c7f105d2e34894cd19905ec945973c
William Babincsak <58189391+TheWillBa@users.noreply.github.com>
2023-04-20 15:23:53 -0400

    run fault scale experiments

--- experiments/fault_scale/generate.bash
+++ experiments/fault_scale/generate.bash
@@ -9,6 +9,7 @@ for fault in "FAULT_PROXIMITYSENSORS_SETRANDOM" "FAULT_ACTUATOR_LWHEEL_SETZERO";
     for n in {1..10}; do
         mkdir $n
         cd $n
+        ln -s ../../../../build build
         # this is for the number of iterations
         for iter in {1..20}; do
             python3 ../../../generate_experiments.py --size 16 --fault $fault --seed $seed --num_faulty $n --led_bins 4 --length 300 --lower 0 --upper 451 --headless
